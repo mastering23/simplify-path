de5ed7b4a8952cae24e6a3a63537d7c5c5251f50
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-21 01:09:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+Simplify path
+("/home/");                   // output : /home
+("/x/./y/xx/xx/z/");         //  output : /z
+("/../")                     // output : /
+("/home//test/");          // output: /home/test
+
+*/
+
+
